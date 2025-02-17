ad2baf6b94289e2567bbc458076b9a1e378859a6
TrudeEH <ehtrude@gmail.com>
2025-02-17 14:54:25 +0000

    Refactor navigation styles and update logo click behavior

--- static/portfolio/index.js
+++ static/portfolio/index.js
@@ -1,20 +1,22 @@
-const navElements = document.querySelectorAll("nav #nav-container a");
-pointer = 1;
+// Code to toggle the menu; Disabled to serve as back button On HTML: onclick="menuToggle(this)"
 
-function menuToggle(x) {
-  if (x.classList.contains("menuAlt")) {
-    document.getElementById("nav").classList.add("nav-open");
-    setTimeout(() => {
-      document.getElementById("nav").classList.remove("nav-open");
-      document.getElementById("nav-container").style.display = "flex";
-    }, 200);
-  } else {
-    document.getElementById("nav").classList.add("nav-close");
-    document.getElementById("nav-container").style.display = "none";
-    setTimeout(() => {
-      document.getElementById("nav").classList.remove("nav-close");
-    }, 200);
-  }
-  x.classList.toggle("menuAlt");
-  document.getElementById("nav").classList.toggle("hide");
-}
+// const navElements = document.querySelectorAll("nav #nav-container a");
+// pointer = 1;
+
+// function menuToggle(x) {
+//   if (x.classList.contains("menuAlt")) {
+//     document.getElementById("nav").classList.add("nav-open");
+//     setTimeout(() => {
+//       document.getElementById("nav").classList.remove("nav-open");
+//       document.getElementById("nav-container").style.display = "flex";
+//     }, 200);
+//   } else {
+//     document.getElementById("nav").classList.add("nav-close");
+//     document.getElementById("nav-container").style.display = "none";
+//     setTimeout(() => {
+//       document.getElementById("nav").classList.remove("nav-close");
+//     }, 200);
+//   }
+//   x.classList.toggle("menuAlt");
+//   document.getElementById("nav").classList.toggle("hide");
+// }
